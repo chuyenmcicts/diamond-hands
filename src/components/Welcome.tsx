343291d95ba71dcf7cb0fa777ad46a3c1326547a
chuyenmc <chuyenmc@icts.vn>
2022-06-21 13:47:24 +0700

    fix: padding, font size

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -9,7 +9,7 @@ const component = (props: IProps) => (
         <p className="header-content">Not Your <span className="header-content-key">Keys</span>, Not Your <span className="header-content-key">Coins</span>.</p>
         <p className="content">If your crypto is stored in a wallet you don't have the private keys for, like a wallet on an exchange, is it really yours? <br></br>Many will say it isn't.</p>
         <div className="container-button">
-            <button type="button" className="btn btn-grad" onClick={props.onGetStarted} >Get Started</button>
+            <button type="button" className="btn-grad" onClick={props.onGetStarted} >Get Started</button>
         </div>
     </>
 )
