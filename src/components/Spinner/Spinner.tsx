d947788a3d5ec52121597fc20ff277cc6765ca59
chuyenmc <chuyenmc@icts.vn>
2022-06-24 07:28:25 +0700

    fix ui dialog, homepage

---
+++ src/components/Spinner/Spinner.tsx
@@ -0,0 +1,11 @@
+import './Spinner.css';
+
+function Spinner() {
+
+  return (
+    <div className="loader"><div className="lds-ring"><div></div><div></div><div></div><div></div></div></div>
+
+  );
+}
+
+export default Spinner;
