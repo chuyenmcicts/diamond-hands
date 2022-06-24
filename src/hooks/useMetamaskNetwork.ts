d9b2040b2241059a88e25aaee2db1b05a6ac8295
BinhND <binh@mission.plus>
2022-06-24 18:16:26 +0700

    Detect network change correctly

--- src/hooks/useMetamaskNetwork.ts
+++ src/hooks/useMetamaskNetwork.ts
@@ -2,27 +2,39 @@ import { useEffect, useState } from "react";
 import { chainList } from "../controllers/configs";
 import { ChainInfo } from "../types";
 
+const composeChainInfo = (chainId: number, supportedChainId: number) => ({
+  valid: Number(chainId) === Number(supportedChainId),
+  chainId,
+  name: chainList[Number(chainId)] || "Unknown",
+});
+
 export const useMetamaskNetwork = (supportedChainId: number) => {
   const ethereum = (window as any).ethereum;
   const currentChainId = Number(ethereum ? ethereum.chainId : 0);
 
-  const [chainInfo, setChainInfo] = useState<ChainInfo>({
-    valid: Number(currentChainId) === Number(supportedChainId),
-    chainId: currentChainId,
-    name: chainList[Number(currentChainId)] || "Unknown",
-  });
+  const [chainInfo, setChainInfo] = useState<ChainInfo>(
+    composeChainInfo(currentChainId, supportedChainId)
+  );
 
   useEffect(() => {
     if (ethereum) {
       ethereum.on("chainChanged", (chainId: number) => {
-        setChainInfo({
-          valid: Number(supportedChainId) === Number(chainId),
-          chainId: Number(chainId),
-          name: chainList[Number(chainId)] || "Unknown",
-        });
+        setChainInfo(composeChainInfo(chainId, supportedChainId));
       });
     }
   }, [ethereum, supportedChainId]);
+
+  // Sometime chainId is not available after starting
+  // Need retry again
+  useEffect(() => {
+    let chainId = Number(ethereum ? ethereum.chainId : 0);
+    if (!chainId) {
+      setTimeout(() => {
+        chainId = Number(ethereum ? ethereum.chainId : 0);
+        setChainInfo(composeChainInfo(chainId, supportedChainId));
+      }, 500)
+    }
+  }, [ethereum, supportedChainId])
 
   return chainInfo;
 };
